bfed7e44ee5aad33999fa188c3d82359f27cdc1d
Smthy <61051295+Smthy@users.noreply.github.com>
2021-05-24 12:09:47 +0100

    All levels finished
    All levels have been finished and tested
    Bug fixes and more polish needs to be implemented

---
+++ FreeClimbv2/Assets/Scripts/Timer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    public float time;
+
+    bool timerActive;
+
+    private void Start()
+    {
+        timerActive = false;
+    }
+
+    private void Update()
+    {
+        colourChange();
+
+        if (timerActive)
+        {
+            time += Time.deltaTime;
+
+            timerText.text = ("Time: " + time);
+        }
+        else
+        {
+            timerText.text = ("Time: " + time);
+        }       
+    }
+
+    public void StartTimer()
+    {
+        timerActive = true;
+    }
+
+    public void StopTimer()
+    {
+        timerActive = false;
+    }
+
+    void colourChange()
+    {
+        if(time >= 45)
+        {
+            timerText.color = new Color(0.812f, 0.667f, 0.200f, 1);
+        }
+        else if(time >= 90)
+        {
+            timerText.color = new Color(0.753f, 0.753f, 0.753f, 1);
+        }
+        else if(time >= 145)
+        {
+            timerText.color = new Color(0.804f, 0.500f, 0.200f, 1);
+        }
+        else
+        {
+            timerText.color = Color.white;
+        }
+
+
+    }
+
+
+}
---
+++ FreeClimbv2/Assets/Scripts/UI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+    public void Level1()
+    {
+        SceneManager.LoadScene("Main Game");
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void SpeedRun()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
